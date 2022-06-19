23a95ecee2f1d2334c24b8d817b7100f6166b530
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-19 20:38:45 +0300

    Update eeprom.lua

--- eeprom.lua
+++ eeprom.lua
@@ -4,22 +4,22 @@ local gpu = component.proxy(component.list("gpu")())
 gpu.bind(component.list("screen")(), true)
 gpu.setDepth(4)
 colors = {
-    white = 0xF0F0F0,
-    orange = F2B233,
-    magenta = E57FD8,
-    lightBlue = 99B2F2,
-    yellow = DEDE6C,
-    lime = 7FCC19,
-    pink = F2B2CC,
-    gray = 4C4C4C,
-    lightGray = 999999,
-    cyan = 4C99B2,
-    purple = B266E5,
-    blue = 3366CC,
-    brown = 7F664C,
-    green = 57A64E,
-    red = CC4C4C,
-    black = 191919,
+    white     = 0xF0F0F0,
+    orange    = 0xF2B233,
+    magenta   = 0xE57FD8,
+    lightBlue = 0x99B2F2,
+    yellow    = 0xDEDE6C,
+    lime      = 0x7FCC19,
+    pink      = 0xF2B2CC,
+    gray      = 0x4C4C4C,
+    lightGray = 0x999999,
+    cyan      = 0x4C99B2,
+    purple    = 0xB266E5,
+    blue      = 0x3366CC,
+    brown     = 0x7F664C,
+    green     = 0x57A64E,
+    red       = 0xCC4C4C,
+    black     = 0x191919
 }
 
 local function menu(label, strs, funcs)
