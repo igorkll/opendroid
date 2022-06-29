e7b69143689cb1cc556e406cc5bf5af7e3ee85e5
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-29 21:03:06 +0300

    Update fslink.lua

--- system/libs/fslink.lua
+++ system/libs/fslink.lua
@@ -8,8 +8,40 @@ function fslink.repath(mainpath, path)
     return parts.sconcat(mainpath, path) or mainpath
 end
 
-function fslink.link(fs, path)
+function fslink.link(fs, path, maxOpen, size)
+    local fileCount = 0
+
     local vfs = {}
+
+    vfs.open = function(path, mode)
+        local file, err = fs.open(fslink.repath(path), mode)
+        if file then
+            local closed
+            
+            local obj = {}
+            function obj.read(readCount)
+                if closed then return end
+                return fs.read(file, readCount)
+            end
+            function obj.write(data)
+                if closed then return end
+                return fs.write(file, data)
+            end
+            function obj.close()
+                if closed then return end
+                fileCount = true
+                return fs.close(file)
+            end
+            function obj.seek(...)
+                if closed then return end
+                return fs.seek(file, ...)
+            end
+        end
+        return nil, err
+    end
+    vfs.open = function(path, mode)
+        return fs.open(fslink.repath(path), mode)
+    end
 
     return vfs
 end
